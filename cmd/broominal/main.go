3496ed947513446556773da0a71e0137c757809d
elev1e1n <elev1e1n@gmail.com>
2026-06-15 12:10:26 +0300

    chore: bump version to v1.6.2

--- cmd/broominal/main.go
+++ cmd/broominal/main.go
@@ -15,7 +15,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var Version = "v1.6.1"
+var Version = "v1.6.2"
 
 func init() {
 	cobra.AddTemplateFunc("bold", func(s string) string { return style.Bold + s + style.Reset })
